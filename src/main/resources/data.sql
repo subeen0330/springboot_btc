571c50145a692f7f24c3a6a87885195844171e04
Suyeong.Yoo <ssu0416@gmail.com>
2022-11-15 18:28:08 +0900

    [2022-11-15] 1. H2 Database Config Update 2. Mapper Annotation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into sample (title, contents, createId, createDt, updateId, updateDt) values ('test1', 'test1', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
-insert into sample (title, contents, createId, createDt, updateId, updateDt) values ('test2', 'test2', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
+insert into eo.sample (sample_tit, sample_cont, crt_id, crt_dt, upd_id, upd_dt) values ('test1', 'test1', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
+insert into eo.sample (sample_tit, sample_cont, crt_id, crt_dt, upd_id, upd_dt) values ('test2', 'test2', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
