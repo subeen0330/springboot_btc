af0401762fee75ae4fc0787d6783f15796bb9034
gmlee <gmlee329@naver.com>
2023-03-09 04:55:18 +0900

    initial

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-insert into eo.sample (sample_tit, sample_cont, crt_id, crt_dt, upd_id, upd_dt) values ('test1', 'test1', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
-insert into eo.sample (sample_tit, sample_cont, crt_id, crt_dt, upd_id, upd_dt) values ('test2', 'test2', 'test', CURRENT_TIMESTAMP(), 'test', CURRENT_TIMESTAMP());
